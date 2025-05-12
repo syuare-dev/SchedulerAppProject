24b361e42c2a73e8d4c5be82a51b3964daa09b10
syuare-dev <tachunsa13@gmail.com>
2025-05-12 15:01:00 +0900

    Add: 기능 구현을 위해 임시로 InMemory(HashMap) 생성, 일정 생성 기능 구현

--- src/main/java/com/example/schedulerapp/controller/ScheduleController.java
+++ src/main/java/com/example/schedulerapp/controller/ScheduleController.java
@@ -1,8 +1,17 @@
 package com.example.schedulerapp.controller;
 
+import com.example.schedulerapp.dto.ScheduleRequestDto;
+import com.example.schedulerapp.dto.ScheduleResponseDto;
+import com.example.schedulerapp.entity.Schedule;
 import com.example.schedulerapp.service.ScheduleService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/schedules")
@@ -12,5 +21,30 @@ public class ScheduleController {
 
     public ScheduleController(ScheduleService scheduleService) {
         this.scheduleService = scheduleService;
+    }
+
+    // 메서드 구현 목적 > InMemory 생성
+    private Map<Long, Schedule> scheduleList = new HashMap<>();
+
+
+    /**
+     * 일정 생성 기능
+     * @param requestDto task, authorName, password가 담긴 객체를 매개변수로 하여 요청받도록 함
+     * @return 요청받은 데이터로 객체를 생성 후 반환
+     */
+
+    @PostMapping
+    public ScheduleResponseDto createSchedule(@RequestBody ScheduleRequestDto requestDto) {
+
+        // 식별자(id) 1씩 증가
+        long scheduleId = scheduleList.isEmpty() ? 1 : Collections.max(scheduleList.keySet()) + 1;
+
+        // 요청받은 데이터로 Schedule 객체 생성
+        Schedule schedule = new Schedule(scheduleId, requestDto.getTask(), requestDto.getAuthorName(), requestDto.getPassword());
+
+        // InMemory DB에 Schedule 저장
+        scheduleList.put(scheduleId, schedule);
+
+        return new ScheduleResponseDto(schedule);
     }
 }
