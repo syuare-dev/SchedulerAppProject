21af88dca20efad478564494e8fd1f15aa79c919
syuare-dev <tachunsa13@gmail.com>
2025-05-12 14:59:36 +0900

    Add: 생성자 매개변수 추가(id, password > 요청받아야 하는 데이터), createdDate/modifiedDate > 자동 생성 날짜값으로 추가 Comment: 주석 추가

--- src/main/java/com/example/schedulerapp/entity/Schedule.java
+++ src/main/java/com/example/schedulerapp/entity/Schedule.java
@@ -16,9 +16,23 @@ public class Schedule {
     private LocalDate createdDate;
     private LocalDate modifiedDate;
 
-    public Schedule(String task, String authorName) {
+    /**
+     * 실제 DB에 저장되는 데이터 모델
+     * @param id 식별자 ID
+     * @param task 할일
+     * @param authorName 작성자
+     * @param password 비밀번호
+     * createDate 생성일 (자동 생성)
+     * modifiedDate 수정일 (자동 생성)
+     */
+
+    public Schedule(long id, String task, String authorName, String password) {
+        this.id = id;
         this.task = task;
         this.authorName = authorName;
+        this.password = password;
+        this.createdDate = LocalDate.now();
+        this.modifiedDate = LocalDate.now();
     }
 
 
