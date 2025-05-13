5d813625a5cb1aee7675d64359d0abf83c63f6c4
syuare-dev <tachunsa13@gmail.com>
2025-05-13 22:57:15 +0900

    Add: Author Request/Response DTO 생성

---
+++ src/main/java/com/example/schedulerapp/dto/AuthorRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.schedulerapp.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AuthorRequestDto {
+
+    private String name;
+    private String email;
+}
---
+++ src/main/java/com/example/schedulerapp/dto/AuthorResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.schedulerapp.dto;
+
+import com.example.schedulerapp.entity.Author;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class AuthorResponseDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate createdDate;
+    private LocalDate modifiedDate;
+
+    public AuthorResponseDto(Author author) {
+        this.id = author.getId();
+        this.name = author.getName();
+        this.email = author.getEmail();
+        this.createdDate = author.getCreatedDate();
+        this.modifiedDate = author.getModifiedDate();
+    }
+
+}
